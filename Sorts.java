0e1867294068afa41a5317b68895a70d773c1745
Brandon OConnor <boconnor20@stuy.edu>
2020-12-17 22:59:43 -0500

    adjusted Sorts to accomadate more cases

--- Sorts.java
+++ Sorts.java
@@ -6,10 +6,8 @@ public class Sorts{
   public static void bubbleSort(int[] data){
       for (int i=0; i<data.length; i++){
           for (int j=0; j<data.length-1-i; j++){
-              if (data[j]<data[j+1]){
-                  j++;
-              }
-              else {
+
+              if (data[j]>data[j+1]){
                   int temp=data[j];
                   data[j]=data[j+1];
                   data[j+1]=temp;
