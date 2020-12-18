f2bcd8a5240330627ca9a022a0b9cb6d8346c7db
Brandon OConnor <boconnor20@stuy.edu>
2020-12-17 23:05:08 -0500

    verified that bubbleSort works for various cases

--- SortsTester.java
+++ SortsTester.java
@@ -2,10 +2,11 @@ import java.util.Random;
 import java.util.Arrays;
 public class SortsTester{
   public static void main(String[] args){
-    int[] arr = new int[5];
+    int size=2;
+    int[] arr = new int[size];
     int b = (int)(Math.random()*1000);
     Random rng = new Random(b);
-    for(int i =  0; i < 5; i++ ){
+    for(int i =  0; i < size; i++ ){
       arr[i] = (rng.nextInt() % 1000);
     }
     Sorts tester = new Sorts();
